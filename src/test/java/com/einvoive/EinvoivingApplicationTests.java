c3b7b0b11a1cec66fff7704243741511483b46c6
junaidp <Goandgone1@>
2021-11-02 21:31:24 +0100

    Translation added

--- src/test/java/com/einvoive/EinvoivingApplicationTests.java
+++ src/test/java/com/einvoive/EinvoivingApplicationTests.java
@@ -2,9 +2,11 @@ package com.einvoive;
 
 import com.einvoive.helper.InvoiceHelper;
 import com.einvoive.model.Invoice;
+import com.einvoive.util.Translator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+//import com.google.cloud.translate.*;
 
 @SpringBootTest
 class EinvoivingApplicationTests {
@@ -23,5 +25,31 @@ class EinvoivingApplicationTests {
         invoiceHelper.save(inv);
         System.out.println("");
     }
+
+
+    //USE THIS
+      @Test
+    void translate()  {
+        try{
+            String translation = Translator.translate("en", "ar", "Hello");
+            System.out.println(translation);
+          }
+        catch(Exception ex)
+        {
+            System.out.println("Error in translation:" + ex);
+        }
+    }
+
+    //DONT USE THIS !
+   /* @Test
+    void translateWithGoogle() {
+        Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+        Translation translation = translate.translate("¡Hola Mundo!");
+        System.out.printf("Translated Text:\n\t%s\n", translation.getTranslatedText());
+
+    }*/
+
+
 
 }
