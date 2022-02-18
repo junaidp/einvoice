5cb9f573aba573b099e87249d15fbd0de0d8f919
junaidp@gmail.com <Goandgone1@>
2022-02-18 15:49:25 +0100

    octa fix

--- src/main/java/com/einvoive/OktaOAuth2WebSecurityConfigurerAdapter.java
+++ src/main/java/com/einvoive/OktaOAuth2WebSecurityConfigurerAdapter.java
@@ -1,20 +1,18 @@
-//package com.einvoive;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//
-//@Configuration
-//class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // all routes protected
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                // enable OAuth2/OIDC
-//                .and()
-//                .oauth2Login();
-//    }
-//}
+package com.einvoive;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // Require authentication for all requests under /api/private
+                .antMatchers("/loginOkta").authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
+}
